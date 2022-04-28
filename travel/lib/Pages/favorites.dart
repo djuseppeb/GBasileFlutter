951c41f397a54f448a2bcd804b473ca4dcbdfe50
Giuseppe Basile <48164486+NightsEdge97@users.noreply.github.com>
2022-04-28 15:00:57 +0200

    Pagina con lista preferiti

--- travel/lib/Pages/favorites.dart
+++ travel/lib/Pages/favorites.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travel/components/app_drawer.dart';
+import 'package:travel/components/carousel_title.dart';
+import 'package:travel/components/search_card.dart';
 import 'package:travel/components/top_bar.dart';
 
+import '../models/meta_turistica.dart';
+
 class Favorites extends StatefulWidget {
-  const Favorites({Key? key}) : super(key: key);
+  const Favorites({Key? key,}) : super(key: key);
 
   @override
   State<Favorites> createState() => _FavoritesState();
@@ -12,12 +16,23 @@ class Favorites extends StatefulWidget {
 
 class _FavoritesState extends State<Favorites> {
   List<String> _preferiti = [];
+  final List<MetaTuristica> _listaPreferiti = [];
 
   void initFavoriteList() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     setState(() {
       _preferiti = sp.getStringList('preferiti') ?? [];
+
+      for(int i=0; i < MetaTuristica.listaMete.length; i++){
+        if (_preferiti.contains(MetaTuristica.listaMete[i].city)){
+          _listaPreferiti.add(MetaTuristica.listaMete[i]);
+        }
+      }
+
     });
+
+
+
   }
 
   @override
@@ -29,13 +44,21 @@ class _FavoritesState extends State<Favorites> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TopBar(),
-      drawer: AppDrawer(),
-      body: ListView.builder(
-          itemCount: _preferiti.length,
-          itemBuilder: (context, index) {
-            return Text(_preferiti[index]);
-          },),
+      appBar: const TopBar(),
+      drawer: const AppDrawer(),
+      body: Column(
+        children: [
+          const CarouselTitle("Preferiti"),
+          Expanded(
+            child: ListView.builder(
+                itemCount: _listaPreferiti.length,
+                itemBuilder: (context, index) {
+                  return SearchCard(_listaPreferiti[index]);
+                  },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
