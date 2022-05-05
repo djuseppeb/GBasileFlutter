b2fa9dbb05ac4d1cecffbad702c8c1aba6b22a44
Giuseppe Basile <48164486+NightsEdge97@users.noreply.github.com>
2022-05-05 10:13:05 +0200

    Aggiunti post al profilo dell'utente

--- social_network/lib/pages/profile.dart
+++ social_network/lib/pages/profile.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:social_network/api/api_user.dart';
 import 'package:social_network/components/bottom_bar.dart';
 
+import '../api/api_post.dart';
 import '../components/intestazione.dart';
+import '../components/post_widget.dart';
+import '../models/post.dart';
+import '../models/post_response.dart';
 import '../models/user.dart';
 
 class Profile extends StatefulWidget {
@@ -14,44 +18,119 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  //Dati intestazione
   late User userData;
-  late Future<User> _future;
+  late Future<User> _futureUser;
 
+  //Dati timeline dei post
+  late List<Post> _listaPost;
+  late int _skipPost;
+  late int _page;
+  late bool _hasMorePost;
+  late Future<List<Post>> _futurePost;
+
+
+
+  //Funzioni di fetch
   Future<User> _fetchUserData(String id) async{
     return userData = await ApiUser.getUserById(id);
   }
 
+  Future<List<Post>> _fetchPostData() async{
+    final PostResponse result = await ApiPost.getPostByUser(widget.userId, page: _page);
+    setState(() {
+      _skipPost = _skipPost + result.limit;
+      _hasMorePost = (result.total - _skipPost) > 0;
+      _page++;
+      _listaPost = _listaPost + result.data;
+    });
+    return _listaPost;
+  }
+
+
+  //InitState
   @override
   void initState() {
-    _future = _fetchUserData(widget.userId);
+    _futureUser = _fetchUserData(widget.userId);
+
+    _listaPost = [];
+    _skipPost = 0;
+    _page = 0;
+    _hasMorePost = false;
+    _futurePost = _fetchPostData();
+
     super.initState();
   }
 
+
+  //Build
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       bottomNavigationBar: const BottomBar(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          //Intestazione Profilo
-          child: FutureBuilder(
-            future: _future,
-            builder: (context, snapshot){
-              if (snapshot.hasData && snapshot.data is User){
-                return Intestazione(userData);
-              }
-              if (snapshot.hasError){
-                return Center(
-                    child: Text("Errore nel caricamento: ${snapshot.error}", style: TextStyle(fontSize: 24),)
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FutureBuilder(
+                future: _futureUser,
+                builder: (context, snapshot){
+                  if (snapshot.hasData && snapshot.data is User){
+                    return Intestazione(userData);
+                  }
+                  if (snapshot.hasError){
+                    return Center(
+                        child: Text("Errore nel caricamento: ${snapshot.error}", style: const TextStyle(fontSize: 24),)
+                    );
+                  }
+                  return const Center(
+                      child: CircularProgressIndicator()
+                  );
+                },
+              ),
+            ),
+
+            //Post dell'utente
+            FutureBuilder(
+              future: _futurePost,
+              builder: (context, snapshot){
+                if(snapshot.hasData && snapshot.data is List<Post>){
+                  final _listPostVisualizzati = (snapshot.data as List<Post>);
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    // itemCount = lunghezza della lista con i dati dei post (+ 1 se ci sono altri post)
+                    itemCount: _listPostVisualizzati.length + (_hasMorePost ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      //Se siamo arrivati a stampare l'ultimo post della lista
+                      if(index == _listPostVisualizzati.length){
+                        // chiamiamo di nuovo la funzione fetchPost per aggiungere un'altra pagina di post alla list
+                        _futurePost = _fetchPostData();
+                        // nel frattempo che carico, mostro un simbolo di attesa
+                        return const Center(child: CircularProgressIndicator(),);
+                      }
+                      //Stampo la card con i dati del post
+                      return PostWidget(_listPostVisualizzati[index]);
+                    },
+                  );
+                }
+
+                if (snapshot.hasError){
+                  return Center(
+                      child: Text("Errore nel caricamento: ${snapshot.error}", style: const TextStyle(fontSize: 24),)
+                  );
+                }
+
+                return const Center(
+                    child: CircularProgressIndicator()
                 );
               }
-              return const Center(
-                  child: CircularProgressIndicator()
-              );
-            },
-          ),
+            )
+          ],
         ),
       )
     );
